67392035008047db279c5fa5a33d8f7539357d44
developmatheus <steamdomatheus6@gmail.com>
2026-06-13 23:44:28 -0300

    Cleanup requireToolAccess import

--- backend/routes/accompaniments.js
+++ backend/routes/accompaniments.js
@@ -5,7 +5,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db');
-const { requireAdmin, requireToolAccess } = require('../middleware/auth');
+const { requireAdmin } = require('../middleware/auth');
 const { notifyClients } = require('../menuEvents');
 const {
   parsePositiveId,
